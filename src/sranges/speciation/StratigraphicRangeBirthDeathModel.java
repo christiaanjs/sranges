b26992964359fb90e66ceca10ae0304a3dfd1cb9
Christiaan Swanepoel <christiaan.j.s@gmail.com>
2018-10-26 16:08:37 +1300

    Final variable value tests for likelihood

--- src/sranges/speciation/StratigraphicRangeBirthDeathModel.java
+++ src/sranges/speciation/StratigraphicRangeBirthDeathModel.java
@@ -4,6 +4,9 @@ import beast.core.Input;
 import beast.core.parameter.RealParameter;
 import beast.evolution.speciation.SABirthDeathModel;
 import beast.evolution.tree.TreeInterface;
+import sranges.tree.StratigraphicRangeTree;
+
+import java.util.stream.DoubleStream;
 
 public class StratigraphicRangeBirthDeathModel extends SABirthDeathModel {
 
@@ -32,7 +35,43 @@ public class StratigraphicRangeBirthDeathModel extends SABirthDeathModel {
     @Override
     public double calculateTreeLogLikelihood(TreeInterface tree){
         updateParameters();
-        throw new RuntimeException("Not implemented");
+        StratigraphicRangeTree srTree = (StratigraphicRangeTree) tree;
+
+        double conditionContribution = Math.log(1 - p(origin)); // Condition on sampling
+
+        double eventContribution = srTree.getNodeStream()
+                .filter(n -> !n.isFake())
+                .mapToDouble(n -> {
+                    if(n.isPsiSample()){
+                        return psi;
+                    } else if(n.isRhoSample()){
+                        return rho;
+                    } else { // Speciation event
+                        return lambda * (n.isSymmetric() ? beta : 1 - beta);
+                    }
+                })
+                .map(x -> Math.log(x))
+                .sum();
+
+        double branchContribution = srTree.getNodeStream() // Node above each branch
+                .filter(n -> !n.isFake())
+                .mapToDouble(n -> {
+                    if(n.isRoot()){
+                        return log_q(origin) - log_q(n.getHeight()); // Root branch
+                    } else {
+                        return 0.0; // TODO
+                    }
+                })
+                .sum();
+
+        double rangeContribution = 0.0;
+
+        double unobservedSpeciationContribution = 0.0;
+
+        return conditionContribution +
+                eventContribution +
+                branchContribution +
+                rangeContribution;
     }
 
     double getC1(){
@@ -47,11 +86,11 @@ public class StratigraphicRangeBirthDeathModel extends SABirthDeathModel {
      * Expose internal constant for testing
      */
     protected double q(double t){
-        return q(t, c1, c2);
+        return  4.0 / q(t, c1, c2);
     }
 
     protected double log_q(double t){
-        return log_q(t, c1, c2);
+        return  Math.log(4.0) - log_q(t, c1, c2);
     }
 
     protected double p(double t){
@@ -63,11 +102,18 @@ public class StratigraphicRangeBirthDeathModel extends SABirthDeathModel {
     }
 
     protected double log_q_tilde_asym(double t){
-        return 0.5 * (t*(lambda + mu + psi) - log_q(t));
+        return 0.5 * (-t*(lambda + mu + psi) + log_q(t));
     }
 
     protected double q_tilde_asym(double t){
-        return Math.sqrt(Math.exp(t * (lambda + mu + psi)) * q(t));
+        return Math.sqrt(Math.exp(-t * (lambda + mu + psi)) * q(t));
     }
 
+    protected double q_tilde(double t){
+        return Math.exp(-(lambda_a + beta * (lambda + mu + psi)) * t) * Math.pow(q_tilde_asym(t), 1 - beta);
+    }
+
+    protected double log_q_tilde(double t){
+        return -(lambda_a + beta*(lambda + mu + psi))*t + (1 - beta)*log_q_tilde_asym(t);
+    }
 }
--- test/sranges/speciation/StratigraphicRangeBirthDeathModelTest.java
+++ test/sranges/speciation/StratigraphicRangeBirthDeathModelTest.java
@@ -18,22 +18,26 @@ public class StratigraphicRangeBirthDeathModelTest {
 
     private static final double ORIGIN = 3.0;
     private static final double LAMBDA = 1.0;
-    private static final double MU = 1.0;
+    private static final double MU = 0.2;
     private static final double RHO = 0.7;
-    private static final double PSI = 0.8;
+    private static final double PSI = 1.1;
+    private static final double LAMBDA_A = 0.4;
+    private static final double BETA = 0.8;
 
-    private StratigraphicRangeBirthDeathModel getSrModel(double origin, double lambda, double mu, double psi, double rho){
+    private static StratigraphicRangeBirthDeathModel getSrModel(double origin, double lambda, double mu, double psi, double rho, double lambda_a, double beta) {
         StratigraphicRangeBirthDeathModel srModel = new StratigraphicRangeBirthDeathModel();
         srModel.setInputValue(srModel.originInput.getName(), Double.toString(origin));
         srModel.setInputValue(srModel.birthRateInput.getName(), Double.toString(lambda));
         srModel.setInputValue(srModel.deathRateInput.getName(), Double.toString(mu));
         srModel.setInputValue(srModel.samplingRateInput.getName(), Double.toString(psi));
-        srModel.setInputValue(srModel.samplingProportionInput.getName(), Double.toString(rho));
+        srModel.setInputValue(srModel.rhoProbability.getName(), Double.toString(rho));
         srModel.setInputValue(srModel.removalProbability.getName(), "0.0");
+        srModel.setInputValue(srModel.anageneticSpeciationRateInput.getName(), Double.toString(lambda_a));
+        srModel.setInputValue(srModel.symmetricSpeciationProbabilityInput.getName(), Double.toString(beta));
         return srModel;
     }
 
-    private StratigraphicRangeTree getSingleSampleRangeTree(String newickFormat, String[] taxonIds){
+    private StratigraphicRangeTree getSingleSampleRangeTree(String newickFormat, String[] taxonIds) {
         String newick = String.format(newickFormat, taxonIds);
         StratigraphicRangeTree tree = TestUtil.constructWithNewick(newick);
         List<StratigraphicRange> ranges = Arrays.stream(taxonIds)
@@ -43,15 +47,17 @@ public class StratigraphicRangeBirthDeathModelTest {
         return tree;
     }
 
-    private StratigraphicRangeTree getUltrametricTree(){
+    private StratigraphicRangeTree getUltrametricTree() {
         String[] taxonIds = new String[]{"A", "B", "C"};
         String newickFormat = "((%s:1.0,%s:1.0):1.5, %s:2.5)";
         StratigraphicRangeTree tree = getSingleSampleRangeTree(newickFormat, taxonIds);
         return tree;
     }
 
+    // Special case tests
+
     @Test
-    public void testSpecialCaseYuleConditionOnOrigin(){
+    public void testSpecialCaseYuleConditionOnOrigin() {
         StratigraphicRangeTree tree = getUltrametricTree();
         tree.initAndValidate();
 
@@ -61,14 +67,14 @@ public class StratigraphicRangeBirthDeathModelTest {
         yuleModel.setInputValue(yuleModel.treeInput.getName(), tree);
         yuleModel.initAndValidate();
 
-        StratigraphicRangeBirthDeathModel srModel = getSrModel(ORIGIN, LAMBDA, 0.0, 0.0, 1.0);
+        StratigraphicRangeBirthDeathModel srModel = getSrModel(ORIGIN, LAMBDA, 0.0, 0.0, 1.0, 0.0, 1.0);
         srModel.setInputValue(srModel.treeInput.getName(), tree);
         srModel.initAndValidate();
 
         assertEquals(yuleModel.calculateTreeLogLikelihood(tree), srModel.calculateTreeLogLikelihood(tree), TestUtil.EPSILON);
     }
 
-    private BirthDeathGernhard08Model getBirthDeathSamplingModel(){
+    private BirthDeathGernhard08Model getBirthDeathSamplingModel() {
         BirthDeathGernhard08Model bdModel = new BirthDeathGernhard08Model();
         bdModel.setInputValue(bdModel.birthDiffRateParameterInput.getName(), Double.toString(LAMBDA - MU));
         bdModel.setInputValue(bdModel.relativeDeathRateParameterInput.getName(), Double.toString(MU / LAMBDA));
@@ -76,12 +82,12 @@ public class StratigraphicRangeBirthDeathModelTest {
         return bdModel;
     }
 
-    private StratigraphicRangeBirthDeathModel getSrModelBirthDeathSamplingSpecialCase(){
-        return getSrModel(ORIGIN, LAMBDA, MU, 0.0, RHO);
+    private static StratigraphicRangeBirthDeathModel getSrModelBirthDeathSamplingSpecialCase() {
+        return getSrModel(ORIGIN, LAMBDA, MU, 0.0, RHO, 0.0, 1.0);
     }
 
     @Test
-    public void testSpecialCaseBirthDeathSamplingConditionOnOrigin(){
+    public void testSpecialCaseBirthDeathSamplingConditionOnOrigin() {
         StratigraphicRangeTree tree = getUltrametricTree();
         tree.initAndValidate();
 
@@ -94,18 +100,35 @@ public class StratigraphicRangeBirthDeathModelTest {
         assertEquals(bdModel.calculateTreeLogLikelihood(tree), srModel.calculateTreeLogLikelihood(tree), TestUtil.EPSILON);
     }
 
-    private static double qNotated(double t, double c1, double c2){
-        return 4.0 * Math.exp(-c1 * t)/
-                Math.pow(Math.exp(-c1 * t) * (1.0 - c2) + (1.0 + c2) , 2.0);
+    private static StratigraphicRangeBirthDeathModel getSrModelFBDSpecialCase() {
+        return getSrModel(ORIGIN, LAMBDA, MU, PSI, RHO, 0.0, 1.0);
+    }
+
+
+    @Test
+    public void testSpecialCaseFBD() {
+        fail();
+    }
+
+    // Variable value tests
+
+
+    private static StratigraphicRangeBirthDeathModel getSrModelMixedMode() {
+        return getSrModel(ORIGIN, LAMBDA, MU, PSI, RHO, LAMBDA_A, BETA);
+    }
+
+    private static double qNotated(double t, double c1, double c2) {
+        return 4.0 * Math.exp(-c1 * t) /
+                Math.pow(Math.exp(-c1 * t) * (1.0 - c2) + (1.0 + c2), 2.0);
     }
 
     // Derivation uses q(t) = 4*exp(-c1*t)/(exp(-c1*t)*(1-c2)+(1+c2))^2
     // sampled-ancestor implementation uses reciprocal
     @Test
-    public void testQReciprocalToNotated(){
+    public void testQNotated() {
         double t = 1.3;
 
-        StratigraphicRangeBirthDeathModel model = getSrModelBirthDeathSamplingSpecialCase();
+        StratigraphicRangeBirthDeathModel model = getSrModelMixedMode();
         StratigraphicRangeTree tree = getUltrametricTree();
         model.setInputValue(model.treeInput.getName(), tree);
         model.initAndValidate();
@@ -114,15 +137,14 @@ public class StratigraphicRangeBirthDeathModelTest {
         double c1 = model.getC1();
         double c2 = model.getC2();
 
-        assertEquals(1.0/qNotated(t, c1, c2), model.q(t), TestUtil.EPSILON);
+        assertEquals(qNotated(t, c1, c2), model.q(t), TestUtil.EPSILON);
     }
 
-    // In case they are implemented separately
     @Test
-    public void testLogP(){
+    public void testLogP() {
         double t = 1.3;
 
-        StratigraphicRangeBirthDeathModel model = getSrModelBirthDeathSamplingSpecialCase();
+        StratigraphicRangeBirthDeathModel model = getSrModelMixedMode();
         StratigraphicRangeTree tree = getUltrametricTree();
         model.setInputValue(model.treeInput.getName(), tree);
         model.initAndValidate();
@@ -132,10 +154,10 @@ public class StratigraphicRangeBirthDeathModelTest {
     }
 
     @Test
-    public void testLogQTilde_asym(){
+    public void testLogQTilde_asym() {
         double t = 1.3;
 
-        StratigraphicRangeBirthDeathModel model = getSrModelBirthDeathSamplingSpecialCase();
+        StratigraphicRangeBirthDeathModel model = getSrModelMixedMode();
         StratigraphicRangeTree tree = getUltrametricTree();
         model.setInputValue(model.treeInput.getName(), tree);
         model.initAndValidate();
@@ -144,15 +166,15 @@ public class StratigraphicRangeBirthDeathModelTest {
         assertEquals(Math.log(model.q_tilde_asym(t)), model.log_q_tilde_asym(t), TestUtil.EPSILON);
     }
 
-    private static double qTilde_asymNotated(double t, double lambda, double mu, double psi, double c1, double c2){
-        return Math.sqrt(Math.exp(-t*(lambda + mu + psi))*qNotated(t, c1, c2));
+    private static double qTilde_asymNotated(double t, double c1, double c2, double lambda, double mu, double psi) {
+        return Math.sqrt(Math.exp(-t * (lambda + mu + psi)) * qNotated(t, c1, c2));
     }
 
     @Test
-    public void testQTilde_asymReciprocalToNotated(){
+    public void testQTilde_asymNotated() {
         double t = 1.3;
 
-        StratigraphicRangeBirthDeathModel model = getSrModelBirthDeathSamplingSpecialCase();
+        StratigraphicRangeBirthDeathModel model = getSrModelMixedMode();
         StratigraphicRangeTree tree = getUltrametricTree();
         model.setInputValue(model.treeInput.getName(), tree);
         model.initAndValidate();
@@ -161,8 +183,56 @@ public class StratigraphicRangeBirthDeathModelTest {
         double c1 = model.getC1();
         double c2 = model.getC2();
 
-        assertEquals(1.0/qTilde_asymNotated(t, c1, c2, PSI, LAMBDA, MU), model.q_tilde_asym(t), TestUtil.EPSILON);
+        assertEquals(qTilde_asymNotated(t, c1, c2, PSI, LAMBDA, MU), model.q_tilde_asym(t), TestUtil.EPSILON);
 
     }
 
+    @Test
+    public void testLogQTilde() {
+        double t = 1.3;
+
+        StratigraphicRangeBirthDeathModel model = getSrModelMixedMode();
+        StratigraphicRangeTree tree = getUltrametricTree();
+        model.setInputValue(model.treeInput.getName(), tree);
+        model.initAndValidate();
+        model.updateParameters();
+
+        assertEquals(Math.log(model.q_tilde(t)), model.log_q_tilde(t), TestUtil.EPSILON);
+
+    }
+
+    private static double qTildeNotated(double t, double c1, double c2, double lambda, double mu, double psi, double lambda_a, double beta) {
+        return Math.exp(-(lambda_a + beta * (lambda + mu + psi)) * t) * Math.pow(qTilde_asymNotated(t, c1, c2, lambda, mu, psi), 1 - beta);
+    }
+
+    @Test
+    public void testQTildeNotated() {
+        double t = 1.3;
+
+        StratigraphicRangeBirthDeathModel model = getSrModelMixedMode();
+        StratigraphicRangeTree tree = getUltrametricTree();
+        model.setInputValue(model.treeInput.getName(), tree);
+        model.initAndValidate();
+        model.updateParameters();
+
+        double c1 = model.getC1();
+        double c2 = model.getC2();
+
+        assertEquals(qTildeNotated(t, c1, c2, LAMBDA, MU, PSI, LAMBDA_A, BETA), model.q_tilde(t), TestUtil.EPSILON);
+
+    }
+
+    @Test
+    public void testQTildeSpecialCaseAsym() {
+        double t = 1.3;
+
+        StratigraphicRangeBirthDeathModel model = getSrModel(ORIGIN, LAMBDA, MU, PSI, RHO, 0, 0.0);
+        StratigraphicRangeTree tree = getUltrametricTree();
+        model.setInputValue(model.treeInput.getName(), tree);
+        model.initAndValidate();
+        model.updateParameters();
+
+        assertEquals(model.q_tilde_asym(t), model.q_tilde(t), TestUtil.EPSILON);
+
+    }
 }
